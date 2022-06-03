ca015747812a6677358cae727d69933f387615c6
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-06-03 18:44:15 +0200

    * Created Header component * Created Footer component * installed sass * created variables.scss

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.header_div}></div>
+    </header>
+  );
+};
+
+export default Header;
