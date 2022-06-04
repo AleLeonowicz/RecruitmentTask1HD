feff761ec61c641473d41acba6ca6d450e579027
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-06-04 21:04:41 +0200

    Fixed a bug in carousel btns

--- src/components/CardContent/CardContent.js
+++ src/components/CardContent/CardContent.js
@@ -21,9 +21,8 @@ const CardContent = () => {
 
   const ButtonGroup = ({ next, previous, goToSlide, ...rest }) => {
     const {
-      carouselState: { currentSlide, slidesToShow },
+      carouselState: { currentSlide },
     } = rest;
-    console.log('rest', rest);
 
     return (
       <div className={classes.carouselContainer_buttonGroup}>
@@ -39,7 +38,7 @@ const CardContent = () => {
         </button>
         <button
           className={
-            currentSlide === slidesToShow
+            currentSlide === 2
               ? classes.carouselContainer_buttonRight_disabled
               : classes.carouselContainer_buttonRight
           }
@@ -63,96 +62,10 @@ const CardContent = () => {
     >
       <FirstSlide />
       <SecondSlide />
+
       <div className={classes.cardContent}>3</div>
     </Carousel>
   );
 };
 
 export default CardContent;
-
-// return (
-//     <section className={classes.carousel} aria-label="Gallery">
-//       <ol className={classes.carousel__viewport}>
-//         <li
-//           id="carousel__slide1"
-//           tabIndex="0"
-//           className={classes.carousel__slide}
-//         >
-//           <div className={classes.carousel__snapper}></div>
-//           <a href="#carousel__slide2" className={classes.carousel__next}>
-//             <img
-//               className={classes.carousel__nextBtn}
-//               src={forwardBtn}
-//               alt="Go to previous slide"
-//             />
-//           </a>
-//         </li>
-//         <li
-//           id="carousel__slide2"
-//           tabIndex="0"
-//           className={classes.carousel__slide}
-//         >
-//           <div className={classes.carousel__snapper}></div>
-//           <a href="#carousel__slide1" className={classes.carousel__prev}>
-//             <img
-//               className={classes.carousel__prevBtn}
-//               src={backBtn}
-//               alt="Go to previous slide"
-//             />
-//           </a>
-//           <a href="#carousel__slide3" className={classes.carousel__next}>
-//             <img
-//               className={classes.carousel__nextBtn}
-//               src={forwardBtn}
-//               alt="Go to previous slide"
-//             />
-//           </a>
-//         </li>
-//         <li
-//           id="carousel__slide3"
-//           tabIndex="0"
-//           className={classes.carousel__slide}
-//         >
-//           <div className={classes.carousel__snapper}></div>
-//           <a href="#carousel__slide2" className={classes.carousel__prev}>
-//             <img
-//               className={classes.carousel__prevBtn}
-//               src={backBtn}
-//               alt="Go to previous slide"
-//             />
-//           </a>
-//         </li>
-//       </ol>
-//       <aside className={classes.carousel__navigation}>
-//         <ol className={classes.carousel__navigation__list}>
-//           <li className={classes.carousel__navigation__item}>
-//             <a href="#carousel__slide1">
-//               <img
-//                 className={classes.carousel__navigation_circle}
-//                 src={circle}
-//                 alt="Go to slide 1"
-//               />
-//             </a>
-//           </li>
-//           <li className={classes.carousel__navigation__item}>
-//             <a href="#carousel__slide2">
-//               <img
-//                 className={classes.carousel__navigation_circle}
-//                 src={circle}
-//                 alt="Go to slide 1"
-//               />
-//             </a>
-//           </li>
-//           <li className={classes.carousel__navigation__item}>
-//             <a href="#carousel__slide3">
-//               <img
-//                 className={classes.carousel__navigation_circle}
-//                 src={circle}
-//                 alt="Go to slide 1"
-//               />
-//             </a>
-//           </li>
-//         </ol>
-//       </aside>
-//     </section>
-//   );
