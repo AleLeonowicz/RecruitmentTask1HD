1dabc5116b17f42639efde702936b74e9feb93d3
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-06-04 18:34:42 +0200

    Adjusted and simplified the carousel

--- src/components/CardContent/CardContent.js
+++ src/components/CardContent/CardContent.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import classes from './CardContent.module.scss';
-// import forwardBtn from '../../assets/svg/chevron-forward-outline.svg';
-// import backBtn from '../../assets/svg/chevron-back-outline.svg';
+import forwardBtn from '../../assets/svg/chevron-forward-outline.svg';
+import backBtn from '../../assets/svg/chevron-back-outline.svg';
+import circle from '../../assets/svg/ellipse-outline.svg';
 
 const CardContent = () => {
   return (
@@ -13,11 +14,12 @@ const CardContent = () => {
           className={classes.carousel__slide}
         >
           <div className={classes.carousel__snapper}></div>
-          <a href="#carousel__slide4" className={classes.carousel__prev}>
-            Go to last slide
-          </a>
           <a href="#carousel__slide2" className={classes.carousel__next}>
-            Go to next slide
+            <img
+              className={classes.carousel__nextBtn}
+              src={forwardBtn}
+              alt="Go to previous slide"
+            />
           </a>
         </li>
         <li
@@ -27,10 +29,18 @@ const CardContent = () => {
         >
           <div className={classes.carousel__snapper}></div>
           <a href="#carousel__slide1" className={classes.carousel__prev}>
-            Go to previous slide
+            <img
+              className={classes.carousel__prevBtn}
+              src={backBtn}
+              alt="Go to previous slide"
+            />
           </a>
           <a href="#carousel__slide3" className={classes.carousel__next}>
-            Go to next slide
+            <img
+              className={classes.carousel__nextBtn}
+              src={forwardBtn}
+              alt="Go to previous slide"
+            />
           </a>
         </li>
         <li
@@ -40,58 +50,41 @@ const CardContent = () => {
         >
           <div className={classes.carousel__snapper}></div>
           <a href="#carousel__slide2" className={classes.carousel__prev}>
-            Go to previous slide
-          </a>
-          <a href="#carousel__slide4" className={classes.carousel__next}>
-            Go to next slide
-          </a>
-        </li>
-        <li
-          id="carousel__slide4"
-          tabIndex="0"
-          className={classes.carousel__slide}
-        >
-          <div className={classes.carousel__snapper}></div>
-          <a href="#carousel__slide3" className={classes.carousel__prev}>
-            Go to previous slide
-          </a>
-          <a href="#carousel__slide1" className={classes.carousel__next}>
-            Go to first slide
+            <img
+              className={classes.carousel__prevBtn}
+              src={backBtn}
+              alt="Go to previous slide"
+            />
           </a>
         </li>
       </ol>
       <aside className={classes.carousel__navigation}>
         <ol className={classes.carousel__navigation__list}>
           <li className={classes.carousel__navigation__item}>
-            <a
-              href="#carousel__slide1"
-              className={classes.carousel__navigation__button}
-            >
-              Go to slide 1
+            <a href="#carousel__slide1">
+              <img
+                className={classes.carousel__navigation_circle}
+                src={circle}
+                alt="Go to slide 1"
+              />
             </a>
           </li>
           <li className={classes.carousel__navigation__item}>
-            <a
-              href="#carousel__slide2"
-              className={classes.carousel__navigation__button}
-            >
-              Go to slide 2
+            <a href="#carousel__slide2">
+              <img
+                className={classes.carousel__navigation_circle}
+                src={circle}
+                alt="Go to slide 1"
+              />
             </a>
           </li>
           <li className={classes.carousel__navigation__item}>
-            <a
-              href="#carousel__slide3"
-              className={classes.carousel__navigation__button}
-            >
-              Go to slide 3
-            </a>
-          </li>
-          <li className={classes.carousel__navigation__item}>
-            <a
-              href="#carousel__slide4"
-              className={classes.carousel__navigation__button}
-            >
-              Go to slide 4
+            <a href="#carousel__slide3">
+              <img
+                className={classes.carousel__navigation_circle}
+                src={circle}
+                alt="Go to slide 1"
+              />
             </a>
           </li>
         </ol>
