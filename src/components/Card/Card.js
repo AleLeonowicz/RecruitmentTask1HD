3f8ac2c939b14379e4edbecd979b5b9922402343
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-06-07 20:06:12 +0200

    Card component refactoring

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,9 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import classes from './Card.module.scss';
 import CardHeader from '../CardHeader/CardHeader';
 import CardContent from '../CardContent/CardContent';
 
-import StateContext from '../../store/state-context';
-
 const Card = () => {
-  const { fetchedData } = useContext(StateContext);
   return (
     <section className={classes.card}>
       <CardHeader />
